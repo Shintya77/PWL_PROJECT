8d558fa8db7e2899f5dfa981a6706fd9dd60e846
Shintya77 <shintyarahma77@gmail.com>
2022-05-31 12:32:41 -0700

    edit layout admin

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a href="https://www.bootstrapdash.com/bootstrap-admin/template/template/" target="_blank">Bootstrap admin/template templates</a> from Bootstrapdash.com</span>
+      <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © SiGadai.com 2020</span>
+      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Edit By <a  target="_blank">Anisa Shintya</a> </span>
     </div>
   </footer>
--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -3,7 +3,6 @@
       <a class="sidebar-brand brand-logo" href="index.html"><img src="{{asset('admin/template')}}/assets/images/logo.svg" alt="logo" /></a>
       <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="{{asset('admin/template')}}/assets/images/logo-mini.svg" alt="logo" /></a>
     </div> --}}
-    
     <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
      <h1>SIGADAI</h1>
     </div>
