352191ada83b2f353cea1b954c5fd5b7916fe082
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2022-05-31 11:04:55 +0700

    edit UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('user.home', ['title'=> 'home']);
+    }
+
+    public function about()
+    {
+        return view('user.about', ['title'=> 'about']);
+    }
+
+    public function service()
+    {
+        return view('user.service', ['title'=> 'service']);
+    }
+
+    public function blog()
+    {
+        return view('user.blog', ['title'=> 'blog']);
+    }
+
+    public function contact()
+    {
+        return view('user.contact', ['title'=> 'contact']);
+    }
 }
