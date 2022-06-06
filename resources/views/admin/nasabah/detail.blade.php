3dd3e54b3b8299e4242d9f1993eab32c8158a1c2
Shintya77 <shintyarahma77@gmail.com>
2022-06-05 23:37:49 -0700

    edit detail nasabah

--- resources/views/admin/nasabah/detail.blade.php
+++ resources/views/admin/nasabah/detail.blade.php
@@ -6,19 +6,45 @@
       <div class="card card-info">
         <div class="card-header">
           <h3 class="card-title">
-            <i class="fa fa-table"></i> Tambah Data Nasabah</h3>
+            <i class="fa fa-table"></i> Detail Data Nasabah</h3>
         </div>
         <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item"><b>Id Nasabah: </b>{{$nasabah->Id_Nasabah}}</li>
-                <li class="list-group-item"><b>Nama: </b>{{$nasabah->Nama}}</li>
-                <li class="list-group-item"><b>Username: </b>{{$nasabah->Username}}</li>
-                <li class="list-group-item"><b>TangalLahir: </b>{{$nasabah->TangalLahir}}</li>
-                <li class="list-group-item"><b>JenisKelamin: </b>{{$nasabah->JenisKelamin}}</li>
-                <li class="list-group-item"><b>Usia: </b>{{$nasabah->Usia}}</li>
-                <li class="list-group-item"><b>Alamat: </b>{{$nasabah->Alamat}}</li>
-                <li class="list-group-item"><b>Pekerjaan: </b>{{$nasabah->Pekerjaan}}</li>
-            </ul>
+            <div class="table-responsive ">
+                <table id="example1" class="table table-bordered table-striped">
+                    <tr>
+                        <td>Id Nasabah</td>
+                        <td>{{ $nasabah->Id_Nasabah}}</td>
+                    </tr>
+                    <tr>
+                        <td>Nama Nasabah</td>
+                        <td>{{ $nasabah->Nama }}</td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td>{{ $nasabah->Username }}</td>
+                    </tr>
+                    <tr>
+                        <td>Tanggal Lahir</td>
+                        <td>{{ $nasabah->TangalLahir }}</td>
+                    </tr>
+                    <tr>
+                        <td>Jenis Kelamin</td>
+                        <td>{{ $nasabah->JenisKelamin }}</td>
+                    </tr>
+                    <tr>
+                        <td>Usia</td>
+                        <td>{{ $nasabah->Usia }}</td>
+                    </tr>
+                    <tr>
+                        <td>Alamat</td>
+                        <td>{{ $nasabah->Alamat }}</td>
+                    </tr>
+                    <tr>
+                        <td>Pekerjaan</td>
+                        <td>{{ $nasabah->Pekerjaan }}</td>
+                    </tr>
+                  </table>
+            </div>
         </div>
         <a class="btn btn-success mt-3" href="{{ route('nasabah.index') }}">Kembali</a>
         </div>
