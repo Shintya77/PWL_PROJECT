b94dc98024e5e22e93e8534acd43c3194f808a71
anisarizkinr <nurrahma9b07.sooko1@gmail.com>
2022-06-10 21:43:00 +0700

    membuat crud table pinjaman

---
+++ database/migrations/2022_06_10_143536_create_pinjaman_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePinjamanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pinjaman', function (Blueprint $table) {
+            $table->id();
+            $table->string('Kd_Pinjaman',10)->index();
+            $table->string('Kd_Nasabah',10);
+            $table->string('Kd_Barang',10);
+            $table->string('Pemilik',50);
+            $table->string('Nama',50);
+            $table->integer('JumlahPinjaman');
+            $table->string('JangkaWaktu',50);
+            $table->timestamps();
+
+            
+            $table->foreign('Kd_Nasabah')->references('Kd_Nasabah')->on('Nasabah'); 
+            $table->foreign('Kd_Barang')->references('Kd_Barang')->on('Barang'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pinjaman');
+    }
+}
